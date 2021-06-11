3fb3611faeb7e8c23bb652010d79e6b43929148c
MatthewEspayos <84516496+Matthew-Espayos@users.noreply.github.com>
2021-06-11 17:53:23 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+// Creating variables
+let diameter = 0
+let area = 0
+let circumference = 0
+// Calculates area of a circle
+document.getElementById('mybutton').addEventListener('click', calculate)
+function calculate () {
+  diameter = document.getElementById('diameter').value
+  diameter = parseFloat(diameter / 2).toFixed(2)
+  // Calculates for area
+  myarea = 0
+  area = (Math.PI * diameter ** 2)
+  document.getElementById('myarea').innerHTML = area
+  // Calculates for circumference
+  mycircumference = 0
+  circumference = (2 * Math.PI * diameter)
+  document.getElementById('mycircumference').innerHTML = circumference
+}
